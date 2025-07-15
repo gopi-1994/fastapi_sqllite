e17f643d2daafc1ff94723ff52ec007a865aec58
Gopinath-at-174881232236 <gopinath.v2507@gmail.com>
2025-07-15 23:55:17 +0530

    Enhance database integration and CRUD operations in FastAPI application

--- database.py
+++ database.py
@@ -3,5 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///./books.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_some_threads": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Depends
 from pydantic import BaseModel, Field
 from uuid import UUID, uuid4
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
 class Book(BaseModel):
-    id: UUID 
+    # id: UUID 
     title: str = Field(min_length=1)
     author: str = Field(min_length=1, max_length=100)
     description: str = Field(min_length=1, max_length=500)
@@ -15,34 +27,49 @@ class Book(BaseModel):
 BOOKS = []
 
 @app.get('/')
-async def get_books():
-    return BOOKS
+async def get_books(db: Session = Depends(get_db)):
+    return db.query(models.Books).all()
 
 @app.post('/')
-async def create_book(book: Book):
-    book.id = uuid4()
-    BOOKS.append(book)
+async def create_book(book: Book, db: Session = Depends(get_db)):
+    # book.id = uuid4()
+    book_model = models.Books()
+    book_model.title = book.title
+    book_model.author = book.author
+    book_model.description = book.description
+    book_model.rating = book.rating
+    
+    db.add(book_model)
+    db.commit()
+    db.refresh(book_model)
     return book
 
 @app.put('/{book_id}')
-async def update_book(book_id: UUID, book: Book):
-    counter = 0
-    for b in BOOKS:
-        if b.id == book_id:
-            BOOKS[counter] = book
-            return BOOKS[counter]
-        counter += 1
-    raise HTTPException(status_code=status.HTTP_404, detail="Book not found")
+async def update_book(book_id: int, book: Book, db: Session = Depends(get_db)):
+    book_model = db.query(models.Books).filter(models.Books.id == book_id).first()
+    
+    if book_model is None:
+        raise HTTPException(status_code=status.HTTP_404, detail="Book not found")
+    else:    
+        book_model.title = book.title
+        book_model.author = book.author
+        book_model.description = book.description
+        book_model.rating = book.rating
+        
+        db.commit()
+        db.refresh(book_model)
+        return book_model
+    
 
 @app.delete('/{book_id}')
-async def delete_book(book_id: UUID):
-    counter = 0
-    for b in BOOKS:
-        if b.id == book_id:
-            del BOOKS[counter]
-            return {"message": "Book deleted successfully"}
-        counter += 1
-    raise HTTPException(status_code=status.HTTP_404, detail="Book not found")
+async def delete_book(book_id: int, db: Session = Depends(get_db)):
+    book_model = db.query(models.Books).filter(models.Books.id == book_id).first()
+    if book_model is None:
+        raise HTTPException(status_code=status.HTTP_404, detail="Book not found")
+    
+    db.delete(book_model)
+    db.commit()
+    return {"message": f"{book_model.title} Book deleted successfully"}
 
 
 
